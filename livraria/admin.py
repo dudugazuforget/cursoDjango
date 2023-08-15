2296b94675b2f87ea507e9fb071b922fa69c2560
dudugazuforget <dudu99744@gmail.com>
2023-08-15 13:46:08 -0400

    foreignkey association

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from livraria.models import Categoria, Editora
+from livraria.models import Autor, Categoria, Editora, Livro
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
+admin.site.register(Autor)
+admin.site.register(Livro)
 
 # Register your models here.
